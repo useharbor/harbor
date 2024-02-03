875a9fc38c9b47eb58f8183b41430c73b7bb3891
Yerim Song <yerim1111@gmail.com>
2024-02-03 15:03:10 -0500

    Created profile page for solvers and publishers

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -4,8 +4,6 @@ import { useCurrentUser } from '../UserContext';
 import Logout from './Logout';
 import { ReactComponent as Earth } from '../assets/earth.svg';
 
-
-
 export default function Navbar() {
     const { currentUser, setCurrentUser } = useCurrentUser();
     return (
@@ -25,25 +23,28 @@ export default function Navbar() {
                     </CustomLink>
                 </ul>}
                 <div className="flex items-center">
-                {currentUser == null ? (
+                    {currentUser == null ? (
+                        <ul>
+                        <CustomLink to="/login" className="text-white hover:bg-white hover:text-white border border-white font-semibold py-2 px-4 rounded hover:shadow-md transition duration-300 ease-in-out mx-2">
+                            Login / Sign Up
+                        </CustomLink>
+                        </ul>
+                    ) : (
+                        <ul className="flex items-center space-x-4">
+                        <CustomLink to="/profile" className="text-white hover:bg-white hover:text-white border border-white font-semibold py-2 px-4 rounded hover:shadow-md transition duration-300 ease-in-out">
+                            Profile
+                        </CustomLink>
+                        <Logout />
+                        </ul>
+                    )}
                     <ul>
-                    <CustomLink to="/login" className="text-white hover:bg-white hover:text-white border border-white font-semibold py-2 px-4 rounded hover:shadow-md transition duration-300 ease-in-out mx-2">
-                        Login / Sign Up
-                    </CustomLink>
+                        <CustomLink to="/" className="flex items-center text-white hover:bg-white hover:text-white border border-white font-semibold py-1 px-2 rounded hover:shadow-md transition duration-300 ease-in-out mx-2">
+                        <Earth
+                            style={{ width: '20px', height: '20px', marginRight: '6px' }}
+                        />
+                        EN
+                        </CustomLink>
                     </ul>
-                ) : (
-                    <ul>
-                    <Logout />
-                    </ul>
-                )}
-                <ul>
-                    <CustomLink to="/" className="flex items-center text-white hover:bg-white hover:text-white border border-white font-semibold py-1 px-2 rounded hover:shadow-md transition duration-300 ease-in-out mx-2">
-                    <Earth
-                        style={{ width: '20px', height: '20px', marginRight: '6px' }}
-                    />
-                    EN
-                    </CustomLink>
-                </ul>
                 </div>
             </nav>
         </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,7 @@ module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}",],
   theme: {
     extend: {
-      backgroundColor: {
+      colors: {
         'cobalt-blue': '#0000FF',
       },
     },
