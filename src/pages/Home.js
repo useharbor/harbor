8b9ec557dc790872445bd7552f56c98d34b55b8d
lzheng25 <lyzheng@andrew.cmu.edu>
2024-02-03 16:03:17 -0500

    Clean up

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -14,7 +14,7 @@ export default function Home() {
           <div className="flex-1 p-20 bg-cobalt-blue text-white">
             <div className="flex flex-col items-left">
               <h1 className="text-[64px] mb-4 font-semibold">Solve</h1>
-              <p className="text-[30px] mb-4">Solve translation problems and get paid out in crypto {currentUser}</p>
+              <p className="text-[30px] mb-4">Solve translation problems and get paid out in crypto</p>
             </div>
           </div>
           <div className="flex-1 p-14 bg-cobalt-blue text-white">
