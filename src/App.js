1f58a2d1bf363b35c0c26126a6c4203f9b2e33e1
jyuwang <joeywang7@gmail.com>
2024-02-03 12:28:39 -0500

    home page edits, jobform publish api changes

--- src/App.js
+++ src/App.js
@@ -2,8 +2,8 @@ import "./App.css";
 import Navbar from "./components/navbar";
 import Home from "./pages/Home";
 import Login from "./pages/Login.js";
-import Solve from './pages/SolveEdit.js';
-import SolveVote from './pages/SolveVote.js';
+import Solve from "./pages/SolveEdit.js";
+import SolveVote from "./pages/SolveVote.js";
 import Publish from "./pages/Publish.js";
 import { Routes, Route } from "react-router-dom";
 
@@ -15,8 +15,8 @@ function App() {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/login" element={<Login />} />
-          <Route path='/solve' element={<Solve />} />
-          <Route path='/solve-vote' element={<SolveVote />} />
+          <Route path="/solve" element={<Solve />} />
+          <Route path="/solve-vote" element={<SolveVote />} />
           <Route path="/publish" element={<Publish />} />
         </Routes>
       </div>
