a1a0ac80e49d1657a8719f8ab69b4c34e5b1cbda
Michael Zhou <michaelzhou@michaels-mbp-15.mynetworksettings.com>
2024-02-02 23:56:51 -0500

    seeding function

--- functions/index.js
+++ functions/index.js
@@ -10,7 +10,8 @@
 const {onRequest} = require("firebase-functions/v2/https");
 const logger = require("firebase-functions/logger");
 const {initializeApp} = require("firebase-admin/app");
-const {getFirestore, Timestamp} = require("firebase-admin/firestore");
+const {getFirestore} = require("firebase-admin/firestore");
+const {Translate} = require('@google-cloud/translate').v2;
 
 // Create and deploy your first functions
 // https://firebase.google.com/docs/functions/get-started
@@ -21,6 +22,23 @@ const {getFirestore, Timestamp} = require("firebase-admin/firestore");
 // });
 
 initializeApp();
+const translate = new Translate();
+
+exports.seeding = onRequest(async (request, response) => {
+    try {
+        // parse out text from the request
+        const data = request.query;
+        const originalText = data.text;
+        console.log("Original Text: ", originalText)
+        // translate the text
+        let [translations] = await translate.translate(originalText, 'en');
+        translations = Array.isArray(translations) ? translations : [translations];
+        response.json({ seed: translations[0] });
+    } catch (error) {
+        console.error('Error during translation:', error);
+        response.status(500).send('Internal Server Error');
+    }
+});
 
 exports.branchingedit = onRequest(async (request, response) => {
     res.send("FILLER");
